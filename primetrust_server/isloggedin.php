b10bab31418bb072c0f46bcdcef60854097c82d4
Ndukiye <110042947+Ndukiye@users.noreply.github.com>
2024-03-29 15:51:18 -0700

    Delete primetrust_server directory

--- primetrust_server/isloggedin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (isset($_COOKIE['isLoggedIn'])) {
-    echo True;
-}else {
-    echo False;
-}
-
--- primetrust_server/logout.php
+++
@@ -1,3 +0,0 @@
-<?php
-setcookie("isLoggedIn", "", time() - 3600, "/");
-header("Location: ../html/dashboard/auth/sign-in.html");
